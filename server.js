72051d93557824ecb828e5eb41e4837899a80f63
Webste <steven.weber@orbis.de>
2018-08-02 16:25:48 +0200

    Markdown editor implementiert

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true}))
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
-
+app.use(express.static(__dirname + '/public'));
 var postSchema = new mongoose.Schema({ 
     title: String,
     summary: String,
