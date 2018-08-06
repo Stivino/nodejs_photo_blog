c93fcbc4297bfeaaf656cb3d5f92356d9251b01a
Webste <steven.weber@orbis.de>
2018-08-06 16:44:12 +0200

    Templates angepasst

---
+++ model/post.js
@@ -0,0 +1,17 @@
+/* post.js */
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var postSchema = new Schema({
+    title: String,
+    title_img: String,
+    summary: String,
+    created: Date,
+    content: String,  
+    hearts: Number,
+    views: Number,
+    comments: [{ user: String, email: String, body: String, date: Date }]
+});
+
+var Post = mongoose.model('Posts', postSchema);
+module.exports = Post;
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // Setup
+var Post = require('./model/post');
 var markdown = require('markdown').markdown;
 var express = require('express');
 var app = express();
@@ -6,25 +7,18 @@ var mongoose = require('mongoose')
 var bodyParser = require('body-parser')
 //var pug = require('pug')
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
-
 mongoose.connect("mongodb://localhost:27017/node-blog");
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true}))
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
-var postSchema = new mongoose.Schema({ 
-    title: String,
-    summary: String,
-    date: Date,
-    comments: [{ body: String, date: Date }],
-    body: String
-}
-);
-var Post = mongoose.model('Post', postSchema);
+app.use(express.static(__dirname + '/img'));
+
 // Routes
 app.get("/", (req, res) => {
-    Post.find({}, (err, posts) => {
+    Post.find({ $query: {}, $orderby: { created: 1}}, function(err, posts){
+        if (err) throw err;
       res.render('pages/index', { posts: posts})
    });
 });
@@ -35,53 +29,30 @@ app.get("/pad", (req, res) => {
 });
 
 app.post('/pad', (req, res) => {
-    console.log("inster");
-    var postData = new Post(req.body);
-    // postData.title = res.body.title;
-    // postData.body = res.body.markdown;
-    // postData.date = res.body.date;
-    // postData.summary = res.body.summary;
-    postData.save().then( result => {
-        res.redirect(''); 
-    }).catch(err => {
-        res.status(400).send("Unable to save data");
-        console.log(err);
-    });
+    // var post = new Post({
+    //     title: res.body.title,
+    //     summary: res.body.summary,
+    //     content: res.body.markdown,
+    //     created: res.body.date});
+    console.log(req.body);
+    var post = new Post(req.body);
+    //console.log('Save post to db:' + post);
+    post.save(function(err) {
+        if (err) throw err;
+        console.log('Post saved successfully!');
+      });
+    res.redirect('/pad');
 });
-
-
 
 app.get("/blog/:id", (req, res) => {
-    console.log("hello");
     var id = req.params.id;
-    console.log(id);
-    Post.findById(id, function (err, doc){
+    Post.findById(id, function (err, doc) {
+        if (err) throw err;
         console.log(doc);
-        res.render('pages/post', { post: doc});    
+        res.render('pages/post', { post: doc, markdown: markdown});    
     });
 });
 
-app.get("/markdown", (req, res) => {
-    // var html = markdown.toHTML( "Hello *World*!" +"![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png \"Logo Title Text 1\")");   
- });
-
- app.get("/addPost", (req, res) => {
-    res.render('pages/addPost')
- });
-
-app.post('/addPost', (req, res) => {
-    var postData = new Post(req.body);
-    // postData.title = res.body.title;
-    // postData.body = res.body.body;
-    // postData.date = res.body.date;
-    // postData.summary = res.body.summary;
-    postData.save().then( result => {
-        res.redirect(''); 
-    }).catch(err => {
-        res.status(400).send("Unable to save data");
-        console.log(err);
-    });
-});
 // Listen
 app.listen(3000, () => {
     console.log('Server listing on 3000');
