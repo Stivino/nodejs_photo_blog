31ba5b8cbf45c6ba548f6bdc6477120b10f96457
Stweber <webersteven93@gmail.com>
2018-08-25 22:17:31 +0200

    Nothing special done here.

--- models/photo.js
+++ models/photo.js
@@ -3,14 +3,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 var photoSchema = new Schema({
-    name: String ,
-    img: { data: Buffer, contentType: String }
+    name: String,
+    img: { data: Buffer, contentType: String } ,
+    hashtags: [String]
 });
-
-
-
-
-
-
 
 module.exports = mongoose.model('Photos', photoSchema);
