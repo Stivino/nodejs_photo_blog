3644047d9f116f76db071a0fee0ffab8a75e7ad4
Stweber <webersteven93@gmail.com>
2018-08-25 22:18:35 +0200

    Added some more restrictions.

--- models/post.js
+++ models/post.js
@@ -3,14 +3,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 var postSchema = new Schema({
-    title: { type: String, required: true },
-    title_img: String,
-    summary: String,
-    created: Date,
-    content: String,  
-    hearts: Number,
+    title:     { type: String, required: true },
+    author: {type: Schema.Types.ObjectId, required: true},
+    title_img: {type: String, required: true},
+    summary: {type: String, required: true},
+    created: { type: Date, default: Date.now },
+    content: {type: String, required: true},  
+    votes: Number,
     views: Number,
-    comments: [{ user: String, email: String, body: String, date: Date }]
+    comments: [{ user: String, email: String, body: String, date: Date }],
+    hashtags: [String]
 });
 
 module.exports = mongoose.model('Posts', postSchema);
