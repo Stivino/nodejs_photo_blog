99d25425d31568eb533c8407ff9fa4d233ff341b
Stweber <webersteven93@gmail.com>
2018-09-02 16:41:35 +0200

    Repl. body-parser

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var dateformat = require('dateFormat');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser')
 var logger = require('morgan');
 
 // define routers
@@ -18,15 +19,18 @@ var config = require('./config');
 // express
 var app = express();
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 app.use(express.static(__dirname + '/public'));
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // connecting the database
